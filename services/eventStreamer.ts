4f587f5151a6f5b0cf3f7992731c1b618bce0ddb
emeric-gh <emericbdavid@gmail.com>
2021-08-26 16:22:39 -0400

    added random event generator from valid and non valid schemas

--- services/eventStreamer.ts
+++ services/eventStreamer.ts
@@ -1,8 +1,24 @@
 const { bankTransfers } = require('./bankTransfers.ts');
 const { producer } = require('../producer.ts');
+import { bankTransfersDifferentType } from "./dataGenerators/dataGenDifferentType";
+import { bankTransfersMissingField } from "./dataGenerators/dataGenMissingField";
 
-('use strict');
+/**
+ * @function mixedDataGenerator function randomly chooses One schema and sends the message
+ * @returns 
+ */
+ const mixedDataGenerator = ()=>{
+  //collection of data generators
+   const dataGens = [bankTransfers, bankTransfersDifferentType, bankTransfersMissingField]
+ //choose random data from set
+ 
+   bankTransferObject = dataGens[Math.floor(Math.random() * dataGens.length)]()
+ }
+
 // Declare an object set to the eval result of running the bankTransfers script
-const bankTransferObject = bankTransfers();
+let bankTransferObject;
+
+mixedDataGenerator()
 
 producer('bankTransfers', bankTransferObject, bankTransferObject.eventName);
+
