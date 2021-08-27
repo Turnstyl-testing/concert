bedda987b1dc335295e33bb3571457b593b31d2f
Jae Kim <jaekimr@gmail.com>
2021-08-27 11:54:46 -0400

    resolving merge conflict for producer.ts

--- producer.ts
+++ producer.ts
@@ -1,49 +1,71 @@
-const { Kafka } = require("kafkajs");
+const { Kafka } = require('kafkajs');
 const { Turnstyl } = require("./turnstyl.ts");
 
-let kafkaConnectionIsOpen = false;
+// TO DO: Consider implmenting a callback and some logic to ensure that a stream can be sent and the connection kept open
+// TO DO: Look into the types of objects that can be passed in
+/**
+ * @function passTurnstyl INCOMPLETE - does turnstyl Front Bias processes * this shouldnt be await
+ * @param message Object message to be compared to DB schema
+ */
+const passTurnstyl = function (message) { 
+  //turnt;
+  return message;
+};
 
 const newTS = new Turnstyl();
 newTS.record;
 // newTS.checkMessage()
 
-const producer = async (
+/**
+ * @function producer function that connects to Kafka sends a message then disconnects
+ * @param producerName STRING name of producer
+ * @param message STRING message that will be sent to Kafka
+ * @param topic STRINg name of the topic that message will be posed to on Kafka
+ */
+const producer = async ( 
   producerName: string,
   message: object,
   topic: string
-  // TO DO: Consider implmenting a callback and some logic to ensure that a stream can be sent and the connection kept open
+  
 ) => {
-  try {
-    // Signal to user that producer is running
-    console.log("Producer is operational");
-    newTS.record(topic, message);
-    console.log("now checking schema from DB ");
-    await newTS.checkSchema(topic); // YOLO
-    console.log("this is the turnstyle Schema", newTS.schemas);
-    //Declare a variable kafka assigned to an instance of kafka (door into the kafka brokerage)
-    const kafka = new Kafka({
-      clientId: producerName,
-      brokers: ["kafka:9092"],
-    });
-    // Init the producer on the kafka object
+  
+  //Declare a variable kafka assigned to an instance of kafka (door into the kafka brokerage)
+  const kafka = new Kafka({
+    clientId: producerName,
+    brokers: ['kafka:9092'],
+  });
+  // Signal to user that producer is running
+  console.log('Producer is operational');
 
-    const producer = kafka.producer();
+  // Init the producer on the kafka object
+  const producer = kafka.producer();
+  
+  newTS.record(topic, message);
+  newTS.checkSchema(topic); 
+
+  try {//CONNECTION
     // Connect to the producer
     await producer.connect();
-    // Send our message to topic x
+  } catch (error) {
+    console.log('Producer Connection error: ',error);
+  }
+
+  try {//SEND MESSAGE
     await producer.send({
       topic: topic,
-      // TO DO: Look into the types of objects that can be passed in
+      
       messages: [{ value: JSON.stringify(message) }],
     });
-    // Close connection to the broker
-    await producer.disconnect();
-    // Confirm to use that data has been sent
-    await console.log("Data sent by producer");
-  } catch (err) {
-    console.log(err);
+    console.log('message is:',message)
+  } catch (error) {
+    console.log('error in message send',error)
   }
-};
+
+  console.log('Data sent by producer');
+
+  // Close connection to the broker
+  producer.disconnect();
+  };
 
 export { producer };
 
