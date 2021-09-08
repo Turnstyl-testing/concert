8175c00b8b319d6091751b62fdfdbabde68ef870
Jae Kim <jaekimr@gmail.com>
2021-09-08 17:46:15 -0400

    correct Producername within eventStreamer

--- services/eventStreamer.ts
+++ services/eventStreamer.ts
@@ -1,24 +1,31 @@
 const { bankTransfers } = require('./bankTransfers.ts');
 const { producer } = require('../producer.ts');
-import { bankTransfersDifferentType } from "./dataGenerators/dataGenDifferentType";
-import { bankTransfersMissingField } from "./dataGenerators/dataGenMissingField";
+import { bankTransfersDifferentType } from './dataGenerators/dataGenDifferentType';
+import { bankTransfersMissingField } from './dataGenerators/dataGenMissingField';
 
 /**
  * @function mixedDataGenerator function randomly chooses One schema and sends the message
- * @returns 
+ * @returns
  */
- const mixedDataGenerator = ()=>{
+const mixedDataGenerator = () => {
   //collection of data generators
-   const dataGens = [bankTransfers, bankTransfersDifferentType, bankTransfersMissingField]
- //choose random data from set
- 
-   bankTransferObject = dataGens[Math.floor(Math.random() * dataGens.length)]()
- }
+  const dataGens = [
+    bankTransfers,
+    bankTransfersDifferentType,
+    bankTransfersMissingField,
+  ];
+  //choose random data from set
+
+  bankTransferObject = dataGens[Math.floor(Math.random() * dataGens.length)]();
+};
 
 // Declare an object set to the eval result of running the bankTransfers script
 let bankTransferObject;
 
-mixedDataGenerator()
+mixedDataGenerator();
 
-producer('bankTransfers', bankTransferObject, bankTransferObject.eventName);
-
+producer(
+  'bank_transfer_transactions',
+  bankTransferObject,
+  bankTransferObject.eventName
+);
