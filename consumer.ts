aa9748d1a56e247bb7e588f83eaffd657824201d
Jae Kim <jaekimr@gmail.com>
2021-09-08 17:48:38 -0400

    Tim's stamp & rerouting to new bigQuery ds & table

--- consumer.ts
+++ consumer.ts
@@ -1,14 +1,14 @@
-const { BigQuery } = require("@google-cloud/bigquery");
+const { BigQuery } = require('@google-cloud/bigquery');
 const bigquery = new BigQuery({
-  projectId: "probable-cove-323115",
-  keyFilename: "./bigQueryServiceCredentials.json",
+  projectId: 'probable-cove-323115',
+  keyFilename: './bigQueryServiceCredentials.json',
 });
-const { Kafka } = require("kafkajs");
+const { Kafka } = require('kafkajs');
 
 // Init the kafka connection object
 const kafka = new Kafka({
-  clientId: "bankTransfers",
-  brokers: ["kafka:9092"],
+  clientId: 'bankTransfers',
+  brokers: ['kafka:9092'],
 });
 
 // Error handler for Big Query inserts
@@ -16,8 +16,8 @@ function insertHandler(err, apiResponse) {
   if (err) {
     // An API error or partial failure occurred.
     console.log(err);
-    if (err.name === "PartialFailureError") {
-      console.log("Partial fail: ", err.errors);
+    if (err.name === 'PartialFailureError') {
+      console.log('Partial fail: ', err.errors);
     }
     console.log(apiResponse);
   }
@@ -30,12 +30,14 @@ let rowSentCounter = 0;
 const sendEachMessage = async ({ message }) => {
   // Declare a new variable set to the parsed JSON of message value
   const messageObj = await JSON.parse(message.value);
-  console.log("Event received", messageObj.event_id);
+  console.log('Event received', messageObj.event_id);
   // Connect with BQ and send data to BigQuery
   // TODO: Consider removing hard coded values for production
   // Accessing the relevant table in the target dataset
-  const table = await bigquery.dataset("nygroup5").table("new_test_table");
-  await console.log("Inserting row...");
+  const table = await bigquery
+    .dataset('turnstyl_test_events')
+    .table('bank_transfer_transactions');
+  await console.log('Inserting row...');
 
   // Create an insertion timestamp obj
   const currentTimestamp = new Date(Date.now()).toISOString();
@@ -43,7 +45,7 @@ const sendEachMessage = async ({ message }) => {
   await table.insert(
     {
       event_id: messageObj.event_id,
-      event_timestamp: messageObj.eventTimstamp,
+      event_timestamp: messageObj.eventTimestamp,
       insertion_timestamp: currentTimestamp,
       event_name: messageObj.eventName,
       payload: JSON.stringify(messageObj),
@@ -61,17 +63,17 @@ const autoRetry = async () => {
       // Here we set up our data consumer boilerplate
       let queue: string[] = new Array();
       // Init our consumer
-      const consumer = kafka.consumer({ groupId: "test-group" });
+      const consumer = kafka.consumer({ groupId: 'test-group' });
       //Connect to the consumer
       await consumer.connect();
-      console.log("🏁🏁🏁 consumer connect success");
+      console.log('🏁🏁🏁 consumer connect success');
       // Subscribe to our desired topic
       await consumer.subscribe({
         // TODO: Consider removing hard coded values for production
-        topic: "bank_transfer_transactions",
+        topic: 'bank_transfer_transactions',
         fromBeginning: true,
       });
-      console.log("🔥🔥🔥 consumer subscribe success");
+      console.log('🔥🔥🔥 consumer subscribe success');
       // Run consumer - extracting topic, partition and message and pushing to queue
       await consumer.run({
         eachMessage: sendEachMessage,
@@ -79,7 +81,7 @@ const autoRetry = async () => {
     };
     Consumer();
   } catch {
-    console.log("❤️‍🔥❤️‍🔥❤️‍🔥 Kafka not ready, autoRetry in 5 seconds");
+    console.log('❤️‍🔥❤️‍🔥❤️‍🔥 Kafka not ready, autoRetry in 5 seconds');
     setTimeout(() => {
       autoRetry();
     }, 3000);
